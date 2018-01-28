b338484d71861d2a7359226c256a98200004e9df
Yurii Voitenko <Yurii_Voitenko@epam.com>
2018-01-28 13:48:08 +0200

    Initial commit

---
+++ src/main/java/ua/khpi/voitenko/riskassessment/RiskassessmentApplication.java
@@ -0,0 +1,12 @@
+package ua.khpi.voitenko.riskassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskassessmentApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/khpi/voitenko/riskassessment/RiskassessmentApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.khpi.voitenko.riskassessment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RiskassessmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
