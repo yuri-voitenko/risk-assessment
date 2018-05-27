58c12ac086f0d0d23c97268608f4589025a4a695
Yurii Voitenko <Yurii_Voitenko@epam.com>
2018-05-27 14:43:41 +0300

    Refactored code related to evaluate strategy

--- src/main/java/ua/khpi/voitenko/riskassessment/assessment/strategy/EvaluationStrategy.java
+++ src/main/java/ua/khpi/voitenko/riskassessment/assessment/strategy/EvaluationStrategy.java
@@ -9,9 +9,15 @@ import java.util.Map;
 public interface EvaluationStrategy {
     BigDecimal calculateOverAllImpact(final Map<String, BigDecimal> impactOfGroups);
 
+    Map<FilledRisk, BigDecimal> getImpactOfRisks(final List<FilledRisk> filledRisks);
+
+    Map<FilledRisk, BigDecimal> getMaxImpactOfRisks(final List<FilledRisk> filledRisks);
+
     Map<String, BigDecimal> getImpactOfGroups(final List<FilledRisk> filledRisks);
 
     Map<String, BigDecimal> getMaxImpactOfGroups(final List<FilledRisk> filledRisks);
+
+    Map<String, Integer> getRateOfGroup();
 
     default BigDecimal getOverAllImpact(final List<FilledRisk> filledRisks) {
         return calculateOverAllImpact(getImpactOfGroups(filledRisks));
--- src/main/java/ua/khpi/voitenko/riskassessment/assessment/strategy/impl/AbstractEvaluationStrategy.java
+++ src/main/java/ua/khpi/voitenko/riskassessment/assessment/strategy/impl/AbstractEvaluationStrategy.java
@@ -17,6 +17,9 @@ import static java.util.Objects.isNull;
 import static java.util.stream.Collectors.toMap;
 
 public abstract class AbstractEvaluationStrategy implements EvaluationStrategy {
+
+    private static final int MAX_IMPACT_VALUE = 25;
+
     @Resource
     private RiskGroupService riskGroupService;
     @Resource
@@ -29,42 +32,36 @@ public abstract class AbstractEvaluationStrategy implements EvaluationStrategy {
     }
 
     @Override
+    public Map<FilledRisk, BigDecimal> getImpactOfRisks(List<FilledRisk> filledRisks) {
+        return filledRisks
+                .stream()
+                .collect(toMap(risk -> risk,
+                        risk -> multiplyImpactByCoefficient(risk.calculateImpact(), getGroupName(risk))));
+    }
+
+    @Override
+    public Map<FilledRisk, BigDecimal> getMaxImpactOfRisks(List<FilledRisk> filledRisks) {
+        return filledRisks
+                .stream()
+                .collect(toMap(risk -> risk,
+                        risk -> multiplyImpactByCoefficient(MAX_IMPACT_VALUE, getGroupName(risk))));
+    }
+
+    @Override
     public Map<String, BigDecimal> getImpactOfGroups(List<FilledRisk> filledRisks) {
         Map<String, BigDecimal> impactOfGroups = new HashMap<>();
-        filledRisks.forEach(risk -> processFilledRisk(impactOfGroups, risk, risk.calculateImpact()));
+        filledRisks.forEach(risk -> processFilledRisk(impactOfGroups, getGroupName(risk), risk.calculateImpact()));
         return impactOfGroups;
     }
 
     @Override
     public Map<String, BigDecimal> getMaxImpactOfGroups(final List<FilledRisk> filledRisks) {
         Map<String, BigDecimal> maxImpactOfGroups = new HashMap<>();
-        filledRisks.forEach(risk -> processFilledRisk(maxImpactOfGroups, risk, 25));
+        filledRisks.forEach(risk -> processFilledRisk(maxImpactOfGroups, getGroupName(risk), MAX_IMPACT_VALUE));
         return maxImpactOfGroups;
     }
 
-    private void processFilledRisk(final Map<String, BigDecimal> impactOfGroups, final FilledRisk risk, int impactValue) {
-        final String groupName = risk.getRisk().getGroup().getName();
-        final BigDecimal oldValue = impactOfGroups.get(groupName);
-        final BigDecimal newValue = BigDecimal.valueOf(impactValue).multiply(getCoefficientOfGroup(groupName));
-        impactOfGroups.put(groupName, isNull(oldValue) ? newValue : oldValue.add(newValue));
-    }
-
-    private BigDecimal getCoefficientOfGroup(final String groupName) {
-        return BigDecimal.valueOf(getRateOfGroupByName(groupName))
-                .divide(BigDecimal.valueOf(getSumOfRatesOfGroups()), 3, BigDecimal.ROUND_HALF_UP);
-    }
-
-    private int getSumOfRatesOfGroups() {
-        if (sumOfRatesOfGroups == 0) {
-            sumOfRatesOfGroups = rateOfGroup.values().stream().mapToInt(Integer::intValue).sum();
-        }
-        return sumOfRatesOfGroups;
-    }
-
-    private int getRateOfGroupByName(final String groupName) {
-        return getRateOfGroup().get(groupName);
-    }
-
+    @Override
     public Map<String, Integer> getRateOfGroup() {
         if (isNull(rateOfGroup)) {
             rateOfGroup = riskGroupService
@@ -73,6 +70,36 @@ public abstract class AbstractEvaluationStrategy implements EvaluationStrategy {
                     .collect(toMap(RiskGroup::getName, getRiskGroupRateFromContext()));
         }
         return rateOfGroup;
+    }
+
+    private BigDecimal multiplyImpactByCoefficient(int impactValue, final String groupName) {
+        return BigDecimal.valueOf(impactValue).multiply(getCoefficientOfGroup(groupName));
+    }
+
+    private BigDecimal getCoefficientOfGroup(final String groupName) {
+        return BigDecimal.valueOf(getRateOfGroupByName(groupName))
+                .divide(BigDecimal.valueOf(getSumOfRatesOfGroups()), 3, BigDecimal.ROUND_HALF_UP);
+    }
+
+    private int getRateOfGroupByName(final String groupName) {
+        return getRateOfGroup().get(groupName);
+    }
+
+    private String getGroupName(FilledRisk risk) {
+        return risk.getRisk().getGroup().getName();
+    }
+
+    private void processFilledRisk(final Map<String, BigDecimal> impactOfGroups, final String groupName, int impactValue) {
+        final BigDecimal oldValue = impactOfGroups.get(groupName);
+        final BigDecimal newValue = multiplyImpactByCoefficient(impactValue, groupName);
+        impactOfGroups.put(groupName, isNull(oldValue) ? newValue : oldValue.add(newValue));
+    }
+
+    private int getSumOfRatesOfGroups() {
+        if (sumOfRatesOfGroups == 0) {
+            sumOfRatesOfGroups = rateOfGroup.values().stream().mapToInt(Integer::intValue).sum();
+        }
+        return sumOfRatesOfGroups;
     }
 
     private Function<RiskGroup, Integer> getRiskGroupRateFromContext() {
